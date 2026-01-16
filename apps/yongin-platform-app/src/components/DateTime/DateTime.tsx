0e3ebae70cabd1d63b9d9233a3fd75b6ad5be87b
yjsun <yjsun@pluxity.com>
2026-01-16 11:57:42 +0900

    chore: formatDate함수 수정

--- apps/yongin-platform-app/src/components/DateTime/DateTime.tsx
+++ apps/yongin-platform-app/src/components/DateTime/DateTime.tsx
@@ -1,18 +1,18 @@
 import { cn } from "@pf-dev/ui";
 import { DateTimeProps } from "./types";
 
+const formatDate = (date: Date, format: string) => {
+  return format
+    .replace("YYYY", String(date.getFullYear()))
+    .replace("MM", String(date.getMonth() + 1).padStart(2, "0"))
+    .replace("DD", String(date.getDate()).padStart(2, "0"))
+    .replace("HH", String(date.getHours()).padStart(2, "0"))
+    .replace("mm", String(date.getMinutes()).padStart(2, "0"))
+    .replace("ss", String(date.getSeconds()).padStart(2, "0"));
+};
+
 export function DateTime({ date, format = "YYYY년 MM월 DD일 HH:mm:ss", className }: DateTimeProps) {
   if (!date) return null;
 
-  const formatData = (date: Date, format: string) => {
-    return format
-      .replace("YYYY", String(date.getFullYear()))
-      .replace("MM", String(date.getMonth() + 1).padStart(2, "0"))
-      .replace("DD", String(date.getDate()).padStart(2, "0"))
-      .replace("HH", String(date.getHours()).padStart(2, "0"))
-      .replace("mm", String(date.getMinutes()).padStart(2, "0"))
-      .replace("ss", String(date.getSeconds()).padStart(2, "0"));
-  };
-
-  return <span className={(cn(className), "text-sm 4k:text-4xl")}>{formatData(date, format)}</span>;
+  return <span className={cn("text-sm 4k:text-4xl", className)}>{formatDate(date, format)}</span>;
 }
--- apps/yongin-platform-app/src/pages/home/index.tsx
+++ apps/yongin-platform-app/src/pages/home/index.tsx
@@ -15,13 +15,13 @@ const dashboardTemplate: GridTemplate = {
   columns: 5,
   rows: 3,
   cells: [
-    { id: "cell-1", colStart: 1, colSpan: 1, rowStart: 1, rowSpan: 1 }, // 날씨
-    { id: "cell-2", colStart: 1, colSpan: 1, rowStart: 2, rowSpan: 1 }, // 출역현황
-    { id: "cell-3", colStart: 1, colSpan: 1, rowStart: 3, rowSpan: 1 }, // 공정현황
-    { id: "cell-4", colStart: 2, colSpan: 3, rowStart: 1, rowSpan: 3 }, // 메인 콘텐츠
-    { id: "cell-5", colStart: 5, colSpan: 1, rowStart: 1, rowSpan: 1 }, // 주요 관리사항
-    { id: "cell-6", colStart: 5, colSpan: 1, rowStart: 2, rowSpan: 1 }, // 일일목표
-    { id: "cell-7", colStart: 5, colSpan: 1, rowStart: 3, rowSpan: 1 }, // 공지사항
+    { id: "weather", colStart: 1, colSpan: 1, rowStart: 1, rowSpan: 1 }, // 날씨
+    { id: "workerStats", colStart: 1, colSpan: 1, rowStart: 2, rowSpan: 1 }, // 출역현황
+    { id: "progressStats", colStart: 1, colSpan: 1, rowStart: 3, rowSpan: 1 }, // 공정현황
+    { id: "viewer", colStart: 2, colSpan: 3, rowStart: 1, rowSpan: 3 }, // 메인 콘텐츠
+    { id: "management", colStart: 5, colSpan: 1, rowStart: 1, rowSpan: 1 }, // 주요 관리사항
+    { id: "dailyStats", colStart: 5, colSpan: 1, rowStart: 2, rowSpan: 1 }, // 일일목표
+    { id: "announcement", colStart: 5, colSpan: 1, rowStart: 3, rowSpan: 1 }, // 공지사항
   ],
 };
 
