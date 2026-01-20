40d83143d3eee5bcf46726aebe11a24b8b6e0bcd
yjsun1996 <yjsun@pluxity.com>
2026-01-20 10:53:39 +0900

    #172 - 출역현황 관리 페이지 (#214)
    * feat: 출역현황 관리 페이지 생성,  ag grid 라이브러리 사용, 검색 필터 기능 추가
    
    * feat: select 컴포넌트로 변경
    
    * chore: uuid, filter 영역, 스타일 수정
    
    * chore: 내보내게 버튼 클릭 시 fileName형식 / 값 따옴표 제거
    
    * chore: hook 폴더 생성, console제거, ModuleRegistry 전역으로 분리
    
    * chore: getWorkStatus hooks -> services로 폴더 변경

---
+++ apps/yongin-platform-admin/src/pages/workStatus/services/workstatusService.ts
@@ -0,0 +1,30 @@
+import type { WorkstatusData } from "../types";
+
+const MOCK_DATA: WorkstatusData[] = [
+  {
+    id: "1",
+    inputDate: "2026-01-07",
+    deviceName: "단말기1",
+    todayContent: "토공 작업 진행",
+  },
+  {
+    id: "2",
+    inputDate: "2026-01-07",
+    deviceName: "단말기2",
+    todayContent: "도로공 작업 완료",
+  },
+  {
+    id: "3",
+    inputDate: "2026-01-07",
+    deviceName: "단말기3",
+    todayContent: "비계착 설치 중",
+  },
+];
+
+export const getWorkStatus = (): Promise<WorkstatusData[]> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(MOCK_DATA);
+    }, 1000);
+  });
+};
