a7f51f37fd0c1bac99e51c4fd885d537696c6112
Na <nadk@pluxity.com>
2026-01-22 16:25:57 +0900

    fix : toast 위치 조정

--- apps/yongin-platform-admin/src/App.tsx
+++ apps/yongin-platform-admin/src/App.tsx
@@ -8,7 +8,7 @@ function AppContent() {
   return (
     <>
       <AppRoutes />
-      <Toaster toasts={toasts} onDismiss={dismissToast} position="top-right" />
+      <Toaster toasts={toasts} onDismiss={dismissToast} />
     </>
   );
 }
