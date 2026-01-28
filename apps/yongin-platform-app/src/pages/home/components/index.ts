7fbb2b2bd3d51f268395a1eb72e2013e37a4b0ce
Na <nadk@pluxity.com>
2026-01-28 11:49:25 +0900

    #253 - refactor(app): remove pages folder and finalize SPA structure

--- apps/yongin-platform-app/src/pages/home/components/index.ts
+++
@@ -1,9 +0,0 @@
-export { Weather } from "./Weather";
-export { Attendance } from "./Attendance";
-export { ProcessStatus } from "./ProcessStatus";
-export { MainContent } from "./MainContent";
-export { DailyStats } from "./DailyStats";
-export { Announcement } from "./Announcement";
-export { Management } from "./Management";
-
-export * from "./types";
