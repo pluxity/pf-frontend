d42c344b12c834837f2fd89f07d047095adc20b7
나동규 <111726054+Nadk-Pluxity@users.noreply.github.com>
2026-01-28 12:13:13 +0900

    #253 - refactor(app): yongin-platform-app SPA 폴더 구조 개선 (#257)
    * #253 - refactor(app): rename CenterContainer to MainContainer
    
    * #253 - refactor(app): move home widgets to components/widgets
    
    * #253 - refactor(app): move HomePage to views folder
    
    * #253 - refactor(app): remove pages folder and finalize SPA structure

--- apps/yongin-platform-app/src/components/widgets/index.ts
+++ apps/yongin-platform-app/src/components/widgets/index.ts
@@ -2,8 +2,7 @@ export { Weather } from "./Weather";
 export { Attendance } from "./Attendance";
 export { ProcessStatus } from "./ProcessStatus";
 export { MainContent } from "./MainContent";
+export { Management } from "./Management";
 export { DailyStats } from "./DailyStats";
 export { Announcement } from "./Announcement";
-export { Management } from "./Management";
-
 export * from "./types";
